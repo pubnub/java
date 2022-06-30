c75c1c01ec0723ef07e02e5732cba72347254e35
marcin.cebo <marcin.cebo@pubnub.com>
2022-06-30 12:21:30 +0200

    Added possibility to create PNConfiguration using UserId instead of uuid

---
+++ src/main/java/com/pubnub/api/UserId.java
@@ -0,0 +1,18 @@
+package com.pubnub.api;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.jetbrains.annotations.NotNull;
+
+import static com.pubnub.api.builder.PubNubErrorBuilder.PNERROBJ_USERID_NULL_OR_EMPTY;
+
+public class UserId {
+
+    @Getter
+    private final String value;
+
+    public UserId(@NotNull String value) throws PubNubException {
+        PubNubUtil.require(value != null && !value.isEmpty(), PNERROBJ_USERID_NULL_OR_EMPTY);
+        this.value = value;
+    }
+}
---
+++ src/test/java/com/pubnub/api/PNConfigurationTest.java
@@ -0,0 +1,59 @@
+package com.pubnub.api;
+
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PNConfigurationTest {
+
+    @Test
+    void can_setUserId() throws PubNubException {
+        PNConfiguration pnConfiguration = new PNConfiguration(new UserId("userId01"));
+        UserId newUserId = new UserId("newUserId");
+        pnConfiguration.setUserId(newUserId);
+
+        assertEquals(newUserId.getValue(), pnConfiguration.getUserId().getValue() );
+    }
+
+    @Test
+    void can_getUserId() throws PubNubException {
+        String userId01value = "userId01";
+        PNConfiguration pnConfiguration = new PNConfiguration(new UserId(userId01value));
+        UserId retrievedUserId = pnConfiguration.getUserId();
+
+        assertEquals(userId01value, retrievedUserId.getValue());
+    }
+
+    @Test
+    void should_throw_exception_when_UserId_is_empty_string() {
+        Assertions.assertThrows(PubNubException.class, () -> new PNConfiguration(new UserId("")));
+    }
+
+    @Test
+    void should_throw_exception_when_setting_userId_to_empty_string() {
+        Assertions.assertThrows(PubNubException.class, () -> {
+            PNConfiguration pnConfiguration = new PNConfiguration(new UserId("userId01"));
+            pnConfiguration.setUserId(new UserId(""));
+        });
+    }
+
+    @Test
+    void can_reset_userId_to_non_empty_string() throws PubNubException {
+        PNConfiguration pnConfiguration = new PNConfiguration(new UserId("userId01"));
+        pnConfiguration.setUserId(new UserId("newUserId"));
+    }
+
+    @Test
+    void should_throw_exception_when_uuid_is_empty_string() {
+        Assertions.assertThrows(PubNubException.class, () -> new PNConfiguration(""));
+    }
+
+    @Test
+    void should_throw_exception_when_uuid_is_null_string() {
+        Assertions.assertThrows(PubNubException.class, () -> {
+            new PNConfiguration((String) null);
+        });
+    }
+}
