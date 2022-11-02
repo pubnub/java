695d08e208bae55e38b357a252e4b7932f241f09
marcin.cebo <marcin.cebo@pubnub.com>
2022-11-02 14:00:09 +0100

    Added possibility to add status when setting ChannelMembers

--- src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
+++ src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
@@ -11,7 +11,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
-public abstract class PNUUID {
+public class PNUUID {
+    @Getter
+    private final UUIDId uuid;
+    private String status = null;
+
+    public PNUUID(UUIDId uuid, String status) {
+        this.uuid = uuid;
+        this.status = status;
+    }
+
     @AllArgsConstructor
     @EqualsAndHashCode
     @Getter
@@ -19,21 +28,31 @@ public abstract class PNUUID {
         private String id;
     }
 
-    @Getter
-    private final UUIDId uuid;
-
     public static PNUUID uuid(final String uuid) {
-        return new JustUUID(new UUIDId(uuid));
+        return new UUIDWithoutCustom(new UUIDId(uuid));
     }
+
+    public static PNUUID uuid(final String uuid, String status) {
+        return new UUIDWithoutCustom(new UUIDId(uuid), status);
+    }
+
     public static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom) {
         return new UUIDWithCustom(new UUIDId(uuid), new HashMap<>(custom));
     }
 
+    public static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom, String status) {
+        return new UUIDWithCustom(new UUIDId(uuid), new HashMap<>(custom), status);
+    }
+
     @Getter
     @EqualsAndHashCode(callSuper = true)
-    public static class JustUUID extends PNUUID {
-        JustUUID(UUIDId uuid) {
+    public static class UUIDWithoutCustom extends PNUUID {
+        private UUIDWithoutCustom(UUIDId uuid) {
             super(uuid);
+        }
+
+        private UUIDWithoutCustom(UUIDId uuid, String status) {
+            super(uuid, status);
         }
     }
 
@@ -43,8 +62,13 @@ public abstract class PNUUID {
         @JsonAdapter(CustomPayloadJsonInterceptor.class)
         private final Object custom;
 
-        UUIDWithCustom(UUIDId uuid, Object custom) {
+        private UUIDWithCustom(UUIDId uuid, Object custom) {
             super(uuid);
+            this.custom = custom;
+        }
+
+        private UUIDWithCustom(UUIDId uuid, Object custom, String status) {
+            super(uuid, status);
             this.custom = custom;
         }
     }
