ad23936ebbadcba364a654ae3bd333291718817c
marcin.cebo <marcin.cebo@pubnub.com>
2022-11-02 14:10:47 +0100

    Added possibility to add status when setting ChannelMembers

--- src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
+++ src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class PNUUID {
     @Getter
     private final UUIDId uuid;
+    @Getter
     private String status = null;
 
     public PNUUID(UUIDId uuid, String status) {
