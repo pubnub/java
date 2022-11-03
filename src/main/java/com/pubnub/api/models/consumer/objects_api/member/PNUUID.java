254286d44a06b0599fb8fc95604b3611a39a60e5
marcin.cebo <marcin.cebo@pubnub.com>
2022-11-03 14:53:07 +0100

    Added possibility to add status when setting ChannelMembers
    Changes after code review.

--- src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
+++ src/main/java/com/pubnub/api/models/consumer/objects_api/member/PNUUID.java
@@ -2,75 +2,55 @@ package com.pubnub.api.models.consumer.objects_api.member;
 
 import com.google.gson.annotations.JsonAdapter;
 import com.pubnub.api.models.consumer.objects_api.util.CustomPayloadJsonInterceptor;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RequiredArgsConstructor
-public class PNUUID {
-    @Getter
-    private final UUIDId uuid;
-    @Getter
-    private String status = null;
+public interface PNUUID {
+    @NotNull
+    UUIDId getUuid();
 
-    public PNUUID(UUIDId uuid, String status) {
-        this.uuid = uuid;
-        this.status = status;
+    static PNUUID uuid(final String uuid) {
+        return new UUIDWithoutCustom(new UUIDId(uuid), null);
     }
 
-    @AllArgsConstructor
-    @EqualsAndHashCode
-    @Getter
-    public static class UUIDId {
-        private String id;
-    }
-
-    public static PNUUID uuid(final String uuid) {
-        return new UUIDWithoutCustom(new UUIDId(uuid));
-    }
-
-    public static PNUUID uuid(final String uuid, String status) {
+    static PNUUID uuid(final String uuid, final String status) {
         return new UUIDWithoutCustom(new UUIDId(uuid), status);
     }
 
-    public static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom) {
-        return new UUIDWithCustom(new UUIDId(uuid), new HashMap<>(custom));
+    static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom) {
+        return new UUIDWithCustom(new UUIDId(uuid), new HashMap<>(custom), null);
     }
 
-    public static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom, String status) {
+    static PNUUID uuidWithCustom(final String uuid, final Map<String, Object> custom, final String status) {
         return new UUIDWithCustom(new UUIDId(uuid), new HashMap<>(custom), status);
     }
 
-    @Getter
-    @EqualsAndHashCode(callSuper = true)
-    public static class UUIDWithoutCustom extends PNUUID {
-        private UUIDWithoutCustom(UUIDId uuid) {
-            super(uuid);
-        }
-
-        private UUIDWithoutCustom(UUIDId uuid, String status) {
-            super(uuid, status);
-        }
+    @Data
+    class UUIDId {
+        private final String id;
     }
 
-    @Getter
-    @EqualsAndHashCode(callSuper = true)
-    public static class UUIDWithCustom extends PNUUID {
+    @Data
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    class UUIDWithoutCustom implements PNUUID {
+        @NonNull
+        private final UUIDId uuid;
+        private final String status;
+    }
+
+    @Data
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    class UUIDWithCustom implements PNUUID {
+        @NonNull
+        private final UUIDId uuid;
         @JsonAdapter(CustomPayloadJsonInterceptor.class)
         private final Object custom;
-
-        private UUIDWithCustom(UUIDId uuid, Object custom) {
-            super(uuid);
-            this.custom = custom;
-        }
-
-        private UUIDWithCustom(UUIDId uuid, Object custom, String status) {
-            super(uuid, status);
-            this.custom = custom;
-        }
+        private final String status;
     }
 }
