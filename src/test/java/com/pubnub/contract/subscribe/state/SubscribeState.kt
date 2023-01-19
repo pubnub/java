3c6dc364b4abfa028592d8d1e4de586c49581451
marcin.cebo <marcin.cebo@pubnub.com>
2023-01-19 09:52:09 +0100

    Added Cucumber tests for Publish and Signal containing MessageType and SpaceId. Modified test to comply with new feature files.

--- src/test/java/com/pubnub/contract/subscribe/state/SubscribeState.kt
+++ src/test/java/com/pubnub/contract/subscribe/state/SubscribeState.kt
@@ -1,8 +1,9 @@
 package com.pubnub.contract.subscribe.state
 
 import com.pubnub.api.models.consumer.pubsub.MessageResult
+import java.util.concurrent.CopyOnWriteArrayList
 
 
 class SubscribeState {
-    var messages: List<MessageResult> = mutableListOf()
+    val messages: CopyOnWriteArrayList<MessageResult> = CopyOnWriteArrayList()
 }
